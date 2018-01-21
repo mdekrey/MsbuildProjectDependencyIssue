6ce4166ffb0b615bf9a27d77ec0af4dae00e3614
Matt DeKrey <mattdekrey@gmail.com>
2018-01-21 13:26:12 -0600

    Add project files.

---
+++ MsbuildCoreProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MsbuildCoreProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
